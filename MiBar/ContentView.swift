e45b92156f473dbb45f32550b8f7153bcd32aea6
extrastu <extrastu888@gmail.com>
2026-09-02 14:40:30 +0800

    Initial Commit

---
+++ MiBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiBar
+//
+//  Created by extrastu on 2026/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiBar/MiBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiBarApp.swift
+//  MiBar
+//
+//  Created by extrastu on 2026/9/2.
+//
+
+import SwiftUI
+
+@main
+struct MiBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
